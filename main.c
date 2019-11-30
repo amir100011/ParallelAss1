3c29ca6b8a586f3c9cc96d76caaa24b83a43a867
amir <amirts@post.bgu.ac.il>
2019-11-30 15:37:58 +0200

    before testing

--- main.c
+++ main.c
@@ -75,16 +75,18 @@ void readMatrixValues(int** origMatrix, int row, int col){
     }
 }
 
-void makeFilter(int **origMatrix,int **newMatrix, PGMFileParameters fileParams){
+void makeFilter(int mytid, int fileRows, int fileCols, int numOfRows, int** filter, int** origMatrix){
+    int startRow = numOfRows * (mytid-1);
     double newCellValue = 1,newCellValue2;
     float power = 0.1111111111;
-    for(int currRow = 0; currRow<fileParams.row; currRow++){
-        for (int currCol = 0; currCol < fileParams.col + 2; currCol++) {
+    int filterIdx = 0 ;
+    for(int currRow = startRow; currRow<fileRows; currRow++){
+        for (int currCol = 0; currCol < fileCols + 2; currCol++) {
             for (int rowTmp = currRow - 1; rowTmp < currRow + 2; rowTmp++) {
-                if (rowTmp < 0 || rowTmp > fileParams.row - 1)
+                if (rowTmp < 0 || rowTmp > fileRows - 1)
                     continue;
                 for (int colTmp = currCol- 1; colTmp < currCol + 2; colTmp++) {
-                    if (colTmp < 0 || colTmp > fileParams.col - 1)
+                    if (colTmp < 0 || colTmp > fileCols - 1)
                         continue;
                     int observedCellValue = origMatrix[rowTmp][colTmp];
                     newCellValue = newCellValue * observedCellValue;
@@ -92,12 +94,11 @@ void makeFilter(int **origMatrix,int **newMatrix, PGMFileParameters fileParams){
             }
             newCellValue = pow(newCellValue, power);
             int newCellValueInt = newCellValue;
-            newMatrix[currRow][currCol] = newCellValueInt;
+            filter[filterIdx] = newCellValueInt;
+            filterIdx++;
             newCellValue = 1;
         }
     }
-
-    free (origMatrix);
 }
 
 void writeNewMatrixToFile(int **newMatrix, PGMFileParameters fileParams){
@@ -116,8 +117,7 @@ void writeNewMatrixToFile(int **newMatrix, PGMFileParameters fileParams){
 
 int main(int argc, char **argv) {
     int mytid, nproc;
-    int MASTER = 0;
-    int tag = 99;
+    int **origMatrix;
     MPI_Status status;
 
 
@@ -126,135 +126,86 @@ int main(int argc, char **argv) {
     MPI_Comm_size(MPI_COMM_WORLD, &nproc);
     printf("MPI task ID = %d\n", mytid);
     if (mytid == 0) {//manager
-        int **origMatrix, **newMatrix, numOfProcWaitingReceive = 0;
+        int numOfProcWaitingReceive = 0;
         if (nproc > 1){
             numOfProcWaitingReceive = nproc - 1;
-    }
-        PGMFileParameters fileParams = readPGM();
-        fileParams.numOfRowsForEachProcess = (int)floor(fileParams.row / nproc);
-        int numberOfLinesForManager =  fileParams.row % nproc;
-        origMatrix = malloc(fileParams.row * sizeof *origMatrix);
-        newMatrix = malloc(fileParams.row * sizeof *newMatrix);
-        for (int i = 0; i < fileParams.row; i++) {
-            origMatrix[i] = malloc(fileParams.col * sizeof *origMatrix[i]);
-            newMatrix[i] = malloc(fileParams.col * sizeof *newMatrix[i]);
         }
+        PGMFileParameters fileParams = readPGM();
+
+        origMatrix = malloc(fileParams.row * sizeof *origMatrix);
+        for (int i = 0; i < fileParams.row; i++)
+            origMatrix[i] = malloc(fileParams.col * sizeof *origMatrix[i]);
+        if (numOfProcWaitingReceive == 0)
+            fileParams.numOfRowsForEachProcess = (int)floor(fileParams.row / nproc);
+        else
+            fileParams.numOfRowsForEachProcess = (int)floor(fileParams.row / (nproc - 1));
+        int numberOfLinesForManager =  fileParams.row % nproc;
         int myLongArray[fileParams.row * fileParams.col];
         int sizeForReceive = fileParams.numOfRowsForEachProcess * fileParams.col;
-
-while (numOfProcWaitingReceive > 0){
-    MPI_Recv(myLongArray[fileParams.col * fileParams.numOfRowsForEachProcess *(nproc - numOfProcWaitingReceive)], fileParams.col * fileParams.numOfRowsForEachProcess , MPI_INT, (nproc - numOfProcWaitingReceive), 1, MPI_COMM_WORLD, &status);
-}
+        int fileParamsFOrSend[3] = {fileParams.numOfRowsForEachProcess,fileParams.row,fileParams.col};
 
         readMatrixValues(origMatrix, fileParams.row, fileParams.col);
-        makeFilter(origMatrix, newMatrix, fileParams);
-        writeNewMatrixToFile(newMatrix, fileParams);
+
+
+        if (numOfProcWaitingReceive > 0)
+            MPI_Bcast(fileParamsFOrSend, 3, MPI_INT, 0, MPI_COMM_WORLD);
+
+        while (numOfProcWaitingReceive > 0){
+            MPI_Recv(myLongArray[fileParams.col * fileParams.numOfRowsForEachProcess *(nproc - numOfProcWaitingReceive)], sizeForReceive , MPI_INT, (nproc - numOfProcWaitingReceive), 1, MPI_COMM_WORLD, &status);
+            numOfProcWaitingReceive = numOfProcWaitingReceive - 1;
+        }
+
+        if (numberOfLinesForManager > 0){
+            int filter [numberOfLinesForManager * fileParams.col];
+            makeFilter(nproc, fileParams.row, fileParams.col, numberOfLinesForManager, filter, origMatrix);
+            for (int index = 0; index < numberOfLinesForManager; index++ )
+                myLongArray[fileParams.row - numberOfLinesForManager + index] = filter[index];
+        }
+
+        FILE *fd = fopen("../newMatrix2.txt", "wb");
+
+        for (int i = 0; i < (fileParams.row * fileParams.col); i++) {
+            fprintf(fd, "%d ", myLongArray[i]);
+            if (i > 0 && i % fileParams.col == 0)
+                fprintf(fd, "\n");
+        }
+        fprintf(fd, "\n");
+
+        fclose(fd);
+    }
+    else{//slave
+        int* buf;
+        int count;
+        int source;
+        int err = MPI_Recv(
+                buf, //void *buf
+                count ,//int count
+                MPI_INT,//MPI_Datatype datatype
+                source,//int source,
+                1,// int tag
+                MPI_COMM_WORLD, //MPI_Comm comm
+                &status); //MPI_Status *status
+        if(err != MPI_SUCCESS){
+            printf("MPI_Recv Error %d, process %d exit.",err,mytid);
+            return err;
+        }
+        int numOfRows = buf[0];
+        int fileRows = buf[1];
+        int fileCols = buf[2];
+        int filter[numOfRows*fileCols];
+        makeFilter(mytid, fileRows, fileCols, numOfRows, filter, origMatrix);
+        err = MPI_Send(
+                filter, //void *buf
+                numOfRows*fileCols,//int count
+                MPI_INT,//MPI_Datatype datatype
+                0, // int dest
+                1,// int tag
+                MPI_COMM_WORLD);//MPI_Comm comm
+        if(err != MPI_SUCCESS){
+            printf("MPI_Send Error %d, process %d exit.",err,mytid);
+            return err;
+        }
     }
 
     return 0;
 }
-
-
-
-
-
-//
-//void func(int** origMatrix, int** array, PGMFileParameters file)
-//{
-//
-//    for (int i=0; i<file.row; i++)
-//    {
-//        for (int j=0; j<file.col; j++)
-//        {
-//            array[i][j] = i*j;
-//            origMatrix[i][j] = i*j;
-//        }
-//    }
-//}
-//
-//void funcTmp(int **origMatrix, PGMFileParameters file){
-//    origMatrix =  malloc(file.row * sizeof *origMatrix);
-//    for (int i=0; i<file.row; i++)
-//    {
-//        origMatrix[i] = malloc(file.col * sizeof *origMatrix[i]);
-//    }
-//}
-//
-//int main()
-//{
-//    int rows = 10, cols =15, i;
-//    PGMFileParameters file;
-//    file.row = 10; file.col = 15;
-//    int **origMatrix, **x;
-//
-//    /* obtain values for rows & cols */
-//
-//    /* allocate the array */
-//    x = malloc(rows * sizeof *x);
-//    for (i=0; i<rows; i++)
-//    {
-//        x[i] = malloc(cols * sizeof *x[i]);
-//    }
-//funcTmp(origMatrix,file);
-//    /* use the array */
-//
-//    func(origMatrix, x, file);
-//
-//    /* deallocate the array */
-//    for (i=0; i<rows; i++)
-//    {
-//        free(x[i]);
-//    }
-//    free(x);
-//}
-
-//////
-//////struct Matrix{
-//////    int matrix[3][3];
-//////};
-//////
-//////struct Matrix createMatrix(){
-//////    struct Matrix matrix = {
-//////            {
-//////                    {1, 2, 3},
-//////                    {4, 5, 6},
-//////                    {7, 8, 9}
-//////            }
-//////    };
-//////    return matrix;
-//////}
-//////struct Matrix OrigMatrix = createMatrix();
-//////struct Matrix newMatrix;
-//////for (int i = 0; i<3; i++){
-//////for(int j = 0; j<3; j++){
-//////newMatrix.matrix[i][j] = newCellValue(i, j, OrigMatrix);
-//////}
-//////}
-//////printMatrix(newMatrix);
-//////
-//////
-//////
-//////
-//////void printMatrix(struct Matrix mat){
-//////    for (int i = 0; i<3; i++){
-//////        for(int j = 0; j<3; j++){
-//////            printf("%d      ",mat.matrix[i][j]);
-//////        }
-//////        printf("\n");
-//////    }
-//////}
-//////
-//////int newCellValue(int col, int row, struct Matrix origMat){
-//////    int newCellValue = 1;
-//////    for(int colTmp = col -1; colTmp<col+2; colTmp++){
-//////        if(colTmp < 0 || colTmp > 2)
-//////            continue;
-//////        for(int rowTmp = row -1; rowTmp<row+2; rowTmp++){
-//////            if(rowTmp < 0 || rowTmp > 2)
-//////                continue;
-//////            int observedCellValue = origMat.matrix[colTmp][rowTmp];
-//////            newCellValue = newCellValue * observedCellValue;
-//////        }
-//////    }
-//////    return newCellValue;
-//////}
